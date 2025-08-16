15cc9d4fb8d948d7a54e297caac62e5c92e1a8be
chaoman5 <chaoman5>
2025-08-16 17:34:00 +0900

    ,

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
+console.log(supabaseUrl)
+console.log(supabaseAnonKey)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // 데이터베이스 스키마
